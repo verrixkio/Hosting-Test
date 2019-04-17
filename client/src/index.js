509f74026453fb9ad142541c54d362e624d7a33c
Thomas <thhulsmans@me.com>
2019-04-17 16:16:21 -0600

    added database information and got api talking

---
+++ client/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+// This line may impact the ability for us to access active_admin
+//import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+// Additionaly part to serviceworker
+//serviceWorker.unregister();
